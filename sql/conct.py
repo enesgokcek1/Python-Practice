6bbe64c3951079bc932f44ee1e20e66d7731923f
Enes Salih Gökçek <118908311+enesgokcek1@users.noreply.github.com>
2025-07-12 16:06:25 +0300

    Add files via upload

---
+++ sql/conct.py
@@ -0,0 +1,1 @@
+import mysql.connectormydb = mysql.connector.connect(    host="localhost",    user="root",    password="Enessalih29.34",    database="mydatabase")mycursor = mydb.cursor()mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
