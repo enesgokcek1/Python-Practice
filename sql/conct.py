9d7446be9b0b30c44616e5f098a5134f8f9e662b
Enes Salih Gökçek <118908311+enesgokcek1@users.noreply.github.com>
2025-07-14 14:57:10 +0300

    Update conct.py

--- sql/conct.py
+++ sql/conct.py
@@ -1,1 +1,44 @@
-import mysql.connectormydb = mysql.connector.connect(    host="localhost",    user="root",    password="Enessalih29.34",    database="mydatabase")mycursor = mydb.cursor()mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
+import mysql.connector
+from datetime import datetime
+from connection import mydb  # Bağlantı objesi ayrı dosyadaysa bu doğrudur
+
+class Student:
+    connection = mydb
+    mycursor = connection.cursor()
+
+    def __init__(self, studentNumber, name, surname, birthday, gender):
+        self.studentNumber = studentNumber
+        self.name = name
+        self.surname = surname
+        self.birthday = birthday
+        self.gender = gender
+
+    def save(self):
+        sql = "INSERT INTO Student(studentNumber, Name, Surname, Birthday, Gender) VALUES (%s, %s, %s, %s, %s)"
+        values = (self.studentNumber, self.name, self.surname, self.birthday, self.gender)
+        try:
+            Student.mycursor.execute(sql, values)
+            Student.connection.commit()
+            print(f"{Student.mycursor.rowcount} kayıt eklendi.")
+        except mysql.connector.Error as err:
+            print("Hata:", err)
+
+# Öğrenci listesi
+students = [
+    Student("101", "Ahmet", "Yılmaz", datetime(2005, 5, 7), "E"),
+    Student("102", "Ali", "Yılmaz", datetime(2002, 5, 7), "E"),
+    Student("103", "Ayşe", "Yılmaz", datetime(2006, 5, 7), "K"),
+    Student("104", "Zeynep", "Yılmaz", datetime(2001, 5, 7), "K"),
+    Student("105", "Gülenay", "Yılmaz", datetime(2003, 5, 7), "K"),
+]
+
+# Öğrencileri veritabanına kaydet
+for ogrenci in students:
+    ogrenci.save()
+
+# Bağlantıyı kapat
+Student.connection.close()
+
+
+
+
